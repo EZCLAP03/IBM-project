3dcc869d1d9e127b3cfcd4ad23466a2d3a9fe3dd
AITIK DANDAPAT <aitikdandapat@gmail.com>
2020-07-21 16:45:52 +0530

    Mask Detection

---
+++ Mask Detection/IBM IMAGE.py
@@ -0,0 +1,35 @@
+import cv2
+import sys
+from playsound import playsound
+import json
+from ibm_watson import VisualRecognitionV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+
+########  Connecting to my watson studio to use the trained model
+authenticator = IAMAuthenticator('MZLKAkBFh7heZTu6pQJDaXT5cjQLch71cGc_i2Prx892')
+
+visual_recognition = VisualRecognitionV3(
+    version='2020-07-21',
+    authenticator=authenticator)
+
+visual_recognition.set_service_url('https://api.us-south.visual-recognition.watson.cloud.ibm.com/') 
+
+########   Haarcascade for face detection
+#faceCascade = cv2.CascadeClassifier('C:/Python38/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
+########   Taking live feed from the webcam
+
+with open("H:\Face-Mask-Detection\dataset\WITHOUT MASK IMAGE\WIN_20200719_00_45_02_Pro.jpg", 'rb') as images_file: 
+    classes = visual_recognition.classify(images_file=images_file,threshold='0.6',classifier_ids='DefaultCustomModel_893593661').get_result()
+
+#print(json.dumps(classes, indent=2))    
+l=json.dumps(classes, indent=2)
+print(classes["images"][0]["classifiers"][0]["classes"][0]["class"])
+img = cv2.imread('H:\Face-Mask-Detection\dataset\WITHOUT MASK IMAGE\WIN_20200719_00_45_02_Pro.jpg')
+cv2.putText(img, classes["images"][0]["classifiers"][0]["classes"][0]["class"], (int((img.shape[0])/2), int((img.shape[1])/2)), cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0), 2)
+cv2.imshow("img",img)
+
+
+#H:\mask-detection-master\data\mask\WIN_20200719_00_43_25_Pro.jpg
